e1188f11095846047798dbace2acd3db4f33dbf2
marcosnocetti <marcos.nocetti@ubots.com.br>
2026-04-24 18:46:01 -0300

    fix: add patch method to BackendClient

--- mobius-app/lib/services/backend_client.dart
+++ mobius-app/lib/services/backend_client.dart
@@ -31,5 +31,8 @@ class BackendClient {
   Future<Response> put(String path, {dynamic data}) =>
       _dio.put('$baseUrl$path', data: data);
 
+  Future<Response> patch(String path, {dynamic data}) =>
+      _dio.patch('$baseUrl$path', data: data);
+
   Future<Response> delete(String path) => _dio.delete('$baseUrl$path');
 }
